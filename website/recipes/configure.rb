ff463c36d35158f0cc1c5afce814534f63f279ef
Anthony Caravello <tony@caravello.us>
2023-12-13 12:03:52 -0500

    Syntax error in configure

--- website/recipes/configure.rb
+++ website/recipes/configure.rb
@@ -18,7 +18,7 @@ sites.each do |id|
 	end
 
 	# Website Application Log
-	directory "/var/lib/porkchop/api" do
+	directory "/var/lib/porkchop/api/"+site['name'] do
 		action	:create
 		owner	"apache"
 		recursive	true
