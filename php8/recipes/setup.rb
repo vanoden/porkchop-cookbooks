b99e6d1a0ae2d9b4763cbedb1362ec2e8567bfa5
Anthony Caravello <tony@caravello.us>
2023-12-19 10:44:55 -0500

    Fix php8 recipe for AL2

--- php8/recipes/setup.rb
+++ php8/recipes/setup.rb
@@ -1,94 +1,21 @@
 php_postfix = node["php_version"]
-php_postfix = node["php_version"]
-php_version = "8.2.9"
+php_postfix = "82"
+php_postfix = "82"
 
-old_version = "8.1"
-
-package "php#{old_version}-cli" do
-	action	:remove
-end
-
-package "php#{old_version}-opcache" do
-	action	:remove
-end
-
-package "php#{old_version}-mbstring" do
-	action	:remove
-end
-
-package "php#{old_version}-process" do
-	action	:remove
-end
-
-package "php#{old_version}-pdo" do
-	action	:remove
-end
-
-package "php#{old_version}-fpm" do
-	action	:remove
-end
-
-package "php#{old_version}-xml" do
-	action	:remove
-end
-
-package "php#{old_version}" do
-	action	:remove
-end
-
-package "php#{old_version}-common" do
-	action	:remove
+packages = ["cli","mysqlnd","xml","gd","pdo","mbstring"]
+if (node['platform'].eql?('raspbian'))
+	packages.push("devel","process");
 end
 
 package "php#{php_postfix}" do
 	action	:install
-	version	php_version
-	options	['--allowerasing'] 
+	#version	php_version
+	#options	['--allowerasing'] 
 end
 
-package "php#{php_postfix}-mysqlnd" do
+packages.each do |package|
+package "php#{php_postfix}-#{package}" do
 	action	:install
-	version	php_version
-	options	['--allowerasing'] 
+	#version	php_version
+	#options	['--allowerasing'] 
 end
-package "php#{php_postfix}-xml" do
-	action	:install
-	version	php_version
-	options	['--allowerasing'] 
-end
-package "php#{php_postfix}-gd" do
-	action	:install
-	version	php_version
-	options	['--allowerasing'] 
-end
-package "php#{php_postfix}-pdo" do
-	action	:install
-	version php_version
-	options	['--allowerasing'] 
-end
-package "php#{php_postfix}-mbstring" do
-	action	:install
-	version php_version
-	options	['--allowerasing'] 
-end
-
-if (node['platform'].eql?('raspbian'))
-	# Nothing Yet
-else
-	package "php#{php_postfix}-devel" do
-		action	:install
-		version	php_version
-	end
-	package "php#{php_postfix}-process" do
-		action	:install
-		version	php_version
-	end
-	#package "php-pecl-memcache" do
-	#	action	:install
-	#	version	"7.2.12-1.el7.remi"
-	#end
-	#package "php-pear-XML-Serializer" do
-	#	action	:install
-	#end
-end
-
